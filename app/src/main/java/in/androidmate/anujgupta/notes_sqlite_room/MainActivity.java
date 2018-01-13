6d69d57a26a459bbe7dac290f04ce7522b1c1f6c
Anuj Gupta <junagupta19@gmail.com>
2018-01-13 17:55:30 +0530

    Added NotesAdapter and displayed notes in recyclerview

--- app/src/main/java/in/androidmate/anujgupta/notes_sqlite_room/MainActivity.java
+++ app/src/main/java/in/androidmate/anujgupta/notes_sqlite_room/MainActivity.java
@@ -2,21 +2,50 @@ package in.androidmate.anujgupta.notes_sqlite_room;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import java.util.List;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import in.androidmate.anujgupta.notes_sqlite_room.adapters.NotesAdapter;
+import in.androidmate.anujgupta.notes_sqlite_room.database.DatabaseHandler;
+import in.androidmate.anujgupta.notes_sqlite_room.models.Note;
 
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.rvNotes)
     RecyclerView rvNotes;
 
+    RecyclerView.Adapter adapter;
+    List<Note> notesList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
+        initViews();
+        loadNotes();
 
+    }
+
+    private void initViews() {
+
+        rvNotes.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void loadNotes(){
+        DatabaseHandler db = new DatabaseHandler(this);
+
+        notesList = db.getAllNotes();
+
+        adapter = new NotesAdapter(this,notesList);
+
+        rvNotes.setAdapter(adapter);
 
     }
 
